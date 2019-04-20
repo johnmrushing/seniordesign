1e6b6fa98249f840f91931326885b4ec1db1a63e
Omar Divleli <omar.divleli@ticketmaster.com>
2019-04-20 12:44:10 -0400

    cleaned up code, error checks, etc.

--- obd-decode/obdSerial.py
+++ obd-decode/obdSerial.py
@@ -9,48 +9,55 @@ ser = serial.Serial(
 	stopbits=serial.STOPBITS_ONE,
 	bytesize=serial.EIGHTBITS,
 )
+def init():
+	try:
+		#STEP 1:
+		#Attempt to send 'ATZ'
+		input = 'ATZ'
+		ser.write(input + '\r\n')
+		time.sleep(1)
+		ser.flush()
+		res = ''
+		while(ser.inWaiting() >1):
+			res += ser.read()
+		print("This is serial res")
+		print(res)
+		if res != "ELM327 v1.3a"
+			raise Exception('Did not receive expected response')
+		
+		#STEP 2:
+		#Attempt to send 'ATSP0'
+		input = 'ATSP0'
+		ser.write(input + '\r\n'*)
+		time.sleep(1)
+		ser.flush()
+		res = []
+		i = 0;
+		while(ser.inWaiting() >1):
+			res.append(ser.read()) #= ser.read()
+			i += 1
+		print(res)
+		if res[6:8] != "OK"
+			raise Exception('Did not receive expected response')
+		
+		#STEP 3:
+		#Attempt to send '0100'
+		input = '0100'
+		ser.write(input + '\r\n')
+		#we need to test reducing this
+		time.sleep(5)
+		ser.flush()
+		res = ''
+		while(ser.inWaiting() >1):
+			res += ser.read()
+		print(res)
+		if res[0:2] != "41"
+			raise Exception('Did not receive expected response')
+			
+	except Exception as error:
+		print(repr(error))
 
-input = 'ATZ'
-ser.write(input + '\r\n')
-ser.flush()
-time.sleep(1)
-res = ''
-while(ser.inWaiting() >1):
-	res += ser.read()
-print(res)
-print("This is serial res")
-print(res)
-
-
-input = 'ATSP0'
-ser.write(input + '\r\n')
-time.sleep(1)
-ser.flush()
-res = []
-i = 0;
-while(ser.inWaiting() >0):
-	res.append(ser.read()) #= ser.read()
-	i += 1
-print(res)
-
-input = '0100'
-ser.write(input + '\r\n')
-time.sleep(5)
-ser.flush()
-res = ''
-while(ser.inWaiting() >1):
-	res += ser.read()
-print(res)
-
-while(1):
-	input = '01 0C 0D'
-	ser.write(input + '\r\n')
-	time.sleep(0.1)
-	ser.flush()
-	res = ''
-	while(ser.inWaiting() >1):
-		res += ser.read()
-	#print(res)
+def decode(res):
 	hexnumber = res.split("\r")
 	hexnumber = hexnumber[1].split(" ")
 	for x in range(len(hexnumber)):
@@ -63,5 +70,19 @@ while(1):
 			A = int(hexnumber[x+1],16)
 			MPH = int(A*(.621))
 			print('MPH : '+ repr(MPH))
+			
+def loop():
+	init()
+	while(1):
+		input = '01 0C 0D'
+		ser.write(input + '\r\n')
+		time.sleep(0.1)
+		ser.flush()
+		res = ''
+		while(ser.inWaiting() >1):
+			res += ser.read()
+		decode(res)
+	ser.close()
 
-ser.close()
+if __name__ == '__main__':
+	loop()
