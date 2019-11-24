2f1c83044f901f819777dd8877eeb1abed79d033
Omar Divleli <omar.divleli@ticketmaster.com>
2019-11-24 17:57:36 -0500

    added video socket and profile logic for frontend

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,6 @@
 let express = require('express');
+let fs = require('fs');
 
-// const serial = require('serialport');
-// serial.list((err, ports) => {
-//   console.log(ports)
-// })
-// const port = new serial('COM1')
 
 let app = express();
 let server = require('http').createServer(app);
@@ -32,10 +28,55 @@ io.on('connection', function (socket) {
   });
 
   socket.on('userSelectedCodes',function (data) {
+    //console.log("gotEEEM" + JSON.stringify(data));
     socket.broadcast.emit("selectedCodes",data)
   });
   socket.on('disconnect', function (data){
     console.log("Socket Disconnected");
+  });
+  socket.on('profiles', function () {
+    fs.readdir('./profiles', function(err, items) {
+      if (err) {
+        return console.log('Unable to scan directory: ' + err);
+      }
+      console.log(items);
+      socket.broadcast.emit("frontEndProfiles",items)
+    });
+  });
+  socket.on('profileSave', function (data) {
+    let filePath = './profiles/'+data.profile+'.json';
+    let profile = [{},{},{},{},{},{}];
+    let newData = {listID: data.listID, data: data.data};
+    let newProf = data.listID
+    fs.readFile(filePath, function (err, data) {
+      //3console.log(JSON.parse(data))
+      for( let i in JSON.parse(data)){
+        profile[i] = JSON.parse(data)[i];
+      }
+      profile[newProf] = newData;
+      fs.writeFile(filePath,JSON.stringify(profile),function(err){
+        // throws an error, you could also catch it here
+        if (err) throw err;
+        // success case, the file was saved
+        console.log('Profile saved!');
+      });
+
+    });
+
+  });
+
+  socket.on('getProfile', function (data) {
+    let filePath = './profiles/'+data+'.json';
+    fs.readFile(filePath, function (err, data) {
+      let newData = JSON.parse(data)
+      socket.broadcast.emit("frontEndSavedProfile", newData)
+      console.log("Sent Profile");
+    });
+
+  });
+
+  socket.on('VideoSetting', function (data) {
+     socket.broadcast.emit("backendVideoSettings", data)
   });
 
 });
